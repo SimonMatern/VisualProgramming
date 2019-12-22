16f945db010dfd0f948945addb03e3ae9b29ff8d
semjon <simon.matern@campus.tu-berlin.de>
2019-12-22 13:36:33 +0100

    create graph dynamically

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import  render_template
 
 import os
@@ -15,6 +15,8 @@ os.environ["HADOOP_CONF_DIR"] = "/usr/local/hadoop/etc/hadoop"
 
 warehouse_location = abspath('/user/hive/warehouse')
 #sc = SparkContext(master = "yarn-client")
+
+"""
 sconf = SparkConf().setAll([('master','yarn-client'),('spark.executor.memory', '4g'), ('spark.app.name', 'Spark Updated Conf'), ('spark.executor.cores', '4'), ('spark.cores.max', '4'), ('spark.driver.memory','4g'),('spark.executor.instances', 10), ('spark.sql.warehouse.dir', warehouse_location)])
 spark = SparkSession \
     .builder \
@@ -22,7 +24,7 @@ spark = SparkSession \
     .config(conf=sconf) \
     .enableHiveSupport() \
     .getOrCreate()
-
+"""
 
 app = Flask(__name__)
 
@@ -31,6 +33,12 @@ app = Flask(__name__)
 def hello_world():
     return render_template("ui.html")
 
+@app.route('/get_node_ui', methods=['GET', 'POST'])
+def get_node_ui():
+    if request.method == 'POST':
+        jsdata = request.form['data']
+        print(jsdata)
+    return "RPC-Flask"
 
 if __name__ == '__main__':
     app.run()
