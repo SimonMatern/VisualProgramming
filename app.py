7d8b36296c753522868be88c4d285f1bbbbffecf
Monika <monika.kwiatkowski@campus.tu-berlin.de>
2019-12-22 20:28:40 +0100

    dynamic sidebar using Jinja2 and changed spark configuration

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask import  render_template
-
+import json
 import os
 # -------- Spark imports  --------
 import pyspark
@@ -11,27 +11,35 @@ from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import HiveContext
 # -------- Spark imports  --------
-os.environ["HADOOP_CONF_DIR"] = "/usr/local/hadoop/etc/hadoop"
-
+#os.environ["HADOOP_CONF_DIR"] = "/usr/local/hadoop/etc/hadoop"
+os.environ["SPARK_HOME"]="/home/nodeuser/nfs_share/spark-2.4.4-bin-hadoop2.7"
 warehouse_location = abspath('/user/hive/warehouse')
 #sc = SparkContext(master = "yarn-client")
 
 """
-sconf = SparkConf().setAll([('master','yarn-client'),('spark.executor.memory', '4g'), ('spark.app.name', 'Spark Updated Conf'), ('spark.executor.cores', '4'), ('spark.cores.max', '4'), ('spark.driver.memory','4g'),('spark.executor.instances', 10), ('spark.sql.warehouse.dir', warehouse_location)])
+sconf = SparkConf().setAll([('master','yarn'),('deploy-mode','client'),('spark.executor.memory', '4g'), ('spark.app.name', 'Spark Updated Conf'), ('spark.executor.cores', '4'), ('spark.cores.max', '4'),
+                            ('spark.driver.memory','4g'),('spark.executor.instances', 10), ('spark.sql.warehouse.dir', warehouse_location)])
 spark = SparkSession \
     .builder \
-    .master("yarn-client") \
     .config(conf=sconf) \
     .enableHiveSupport() \
     .getOrCreate()
+
+print("Spark-Session Created!")
+data_sources = spark.sql("show tables in default").toPandas()
+data_sources = data_sources["tableName"].tolist()
+print(data_sources)
 """
 
 app = Flask(__name__)
 
+data = ["data1", "data2", "data3", "data4"]
+
 
 @app.route('/')
 def hello_world():
-    return render_template("ui.html")
+    global data_sources
+    return render_template("ui.html", data=data)
 
 @app.route('/get_node_ui', methods=['GET', 'POST'])
 def get_node_ui():
