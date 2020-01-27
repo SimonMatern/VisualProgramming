a5d36d08e301542cdc9f0794a84bf985f2fa1776
SimonMatern <simon.matern94@gmail.com>
2020-01-27 19:43:28 +0100

    Spark-Workers and Spark-Drivers use the same Python-Version now

--- utils.py
+++ utils.py
@@ -10,11 +10,12 @@ import uuid
 
 def get_spark_Session():
     os.environ["SPARK_HOME"] = "/home/nodeuser/nfs_share/spark-2.4.4-bin-hadoop2.7"
+    os.environ["PYSPARK_PYTHON"] = "/usr/local/anaconda3/bin/python"
     warehouse_location = abspath('/user/hive/warehouse')
     # sc = SparkContext(master = "yarn-client")
 
     sconf = SparkConf().setAll([('master', 'yarn'), ('deploy-mode', 'client'), ('spark.executor.memory', '4g'),
-                                ('spark.app.name', 'Spark Updated Conf'), ('spark.executor.cores', '4'),
+                                ('spark.app.name', 'Flask-Spark'), ('spark.executor.cores', '4'),
                                 ('spark.cores.max', '4'),
                                 ('spark.driver.memory', '4g'), ('spark.executor.instances', 10),
                                 ('spark.sql.warehouse.dir', warehouse_location)])
